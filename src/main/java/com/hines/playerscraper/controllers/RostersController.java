15c051b80b02106ce7239888c039bb95f27cc02c
jonhines-toast <jon.hines@toasttab.com>
2024-03-30 08:30:45 -0400

    default to current year when no year provided

--- src/main/java/com/hines/playerscraper/controllers/RostersController.java
+++ src/main/java/com/hines/playerscraper/controllers/RostersController.java
@@ -6,11 +6,14 @@ import com.hines.playerscraper.services.PlayerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.http.auth.AuthenticationException;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -35,18 +38,19 @@ public class RostersController
         value = "",
         method = RequestMethod.GET)
     public List<TeamRoster> getTeamRosters(
-        @RequestParam(value = "year", required = true) String leagueYear)
+        @RequestParam(value = "year", required = false) String leagueYear)
     {
-        return playerService.getTeamRosters(leagueYear);
+        String yearToUse = StringUtils.isEmpty(leagueYear) ? String.valueOf(LocalDate.now().getYear()) : leagueYear;
+        return playerService.getTeamRosters(yearToUse);
     }
 
 
-    @ApiOperation(value = "Returns all rosters for each team in The League, for a given year")
+    @ApiOperation(value = "Returns all rosters for each team in The League with detailed matchup information for that days games")
     @RequestMapping(
         value = "email",
         method = RequestMethod.GET)
     public ResponseEntity<Object> kickOffEmail(
-        @RequestParam(value = "year", required = true) String leagueYear,
+        @RequestParam(value = "year", required = false) String leagueYear,
         @RequestParam(value = "apiKey", required = true) String apiKey,
         @RequestParam(value = "teamId", required = true) Set<Integer> teamIds)
         throws AuthenticationException
@@ -56,7 +60,8 @@ public class RostersController
         {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("NOPE");
         }
-        playerService.sendMatchupSummaryForTeamsForToday(leagueYear, teamIds);
+        String yearToUse = StringUtils.isEmpty(leagueYear) ? String.valueOf(LocalDate.now().getYear()) : leagueYear;
+        playerService.sendMatchupSummaryForTeamsForToday(yearToUse, teamIds);
         return ResponseEntity.status(200).body("SENT!");
     }
 
@@ -66,7 +71,7 @@ public class RostersController
         value = "email/freeAgentBats",
         method = RequestMethod.GET)
     public ResponseEntity<Object> freeAgentBatsEmailSend(
-        @RequestParam(value = "year", required = true) String leagueYear,
+        @RequestParam(value = "year", required = false) String leagueYear,
         @RequestParam(value = "apiKey", required = true) String apiKey,
         @RequestParam(value = "numPlayers", required = true, defaultValue = "50") int numPlayers)
     {
@@ -75,7 +80,10 @@ public class RostersController
         {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.emptyList());
         }
-        playerService.sendFreeAgentBatsToConsiderEmail(leagueYear, numPlayers);
+        String yearToUse = StringUtils.isEmpty(leagueYear) ? String.valueOf(LocalDate.now().getYear()) : leagueYear;
+        playerService.sendFreeAgentBatsToConsiderEmail(yearToUse, numPlayers);
         return ResponseEntity.status(200).body("SENT!");
     }
+
+
 }
